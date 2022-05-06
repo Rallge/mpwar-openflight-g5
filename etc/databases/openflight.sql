e659affad1ed86276395b6f600ade1b2eeb7efde
Rallge <geraldine.lopez@students.salle.url.edu>
2022-05-06 22:16:53 +0200

    Se creo la tabla flight

--- etc/databases/openflight.sql
+++ etc/databases/openflight.sql
@@ -14,3 +14,18 @@ CREATE TABLE `user`
     `Password` TEXT     NOT NULL,
     PRIMARY KEY (`Id`)
 ) ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `flight`;
+CREATE TABLE `flight`
+(
+    `Id`            CHAR(36)     NOT NULL,
+    `Origin`        TEXT         NOT NULL,
+    `Destination`   TEXT         NOT NULL,
+    `FlightHours`   INT          NOT NULL,
+    `Price`         INT          NOT NULL,
+    `Currency`      CHAR(5)      NOT NULL,
+    `DepartureDate` DATETIME     NOT NULL,
+    `Aircraft`      VARCHAR(50)  NOT NULL,
+    `Airline`       VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (`Id`)
+) ENGINE = InnoDB;
