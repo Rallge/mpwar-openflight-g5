0d477c781a2fc7fd982184ee1455b1964da874ae
Rallge <geraldine.lopez@students.salle.url.edu>
2022-05-12 01:40:19 +0200

    Se cambio de tipo float a int la variable price

--- src/OpenFlight/BookingFlight/Domain/BookingFlight.php
+++ src/OpenFlight/BookingFlight/Domain/BookingFlight.php
@@ -10,20 +10,20 @@ class BookingFlight
     private Uuid $id; //Cod. Reserva
     private Uuid $idFlight;
     private Uuid $idUser;
-    private string $seat;
     private int $price;
+    private string $seat;
     private string $reservationDate;
     private ClassFlight $classFlight;
 
-    public function __construct(Uuid $id, Uuid $idUser,Uuid $idFlight , string $seat, int $price, ClassFlight $classFlight)
+    public function __construct(Uuid $id, Uuid $idUser,Uuid $idFlight, int $price, string $seat, ClassFlight $classFlight)
     {
         $this->id = $id;
         $this->idUser = $idUser;
         $this->idFlight = $idFlight;
-        self::validateEmptySeat($seat);
-        $this->seat = $seat;
         self::validateEmptyPrice($price);
         $this->price = $price;
+        self::validateEmptySeat($seat);
+        $this->seat = $seat;
         $this->reservationDate = (new \DateTimeImmutable())->format('Y-m-d H:i:s');
         $this->classFlight = $classFlight;
     }
@@ -53,19 +53,18 @@ class BookingFlight
     }
 
     /**
-     * @return string
-     */
-    public function seat(): string
-    {
-        return $this->seat;
-    }
-
-    /**
      * @return int
      */
     public function price(): int
     {
         return $this->price;
+    }
+    /**
+     * @return string
+     */
+    public function seat(): string
+    {
+        return $this->seat;
     }
 
     /**
@@ -99,7 +98,7 @@ class BookingFlight
         }
     }
 
-    public static function RegisterBookingFlight(Uuid $id, Uuid $idUser, Uuid $idFlight, string $seat, float $price, ClassFlight $classFlight):BookingFlight{
-        return new self($id,$idUser, $idFlight,$seat,$price,$classFlight);
+    public static function RegisterBookingFlight(Uuid $id, Uuid $idUser, Uuid $idFlight, int $price, string $seat, ClassFlight $classFlight):BookingFlight{
+        return new self($id,$idUser, $idFlight,$price,$seat,$classFlight);
     }
 }
